24aefac7c0594523752257fa1fd545c3630f4268
Crisandry Javier <crisandryj@gmail.com>
2022-04-26 09:21:33 -0400

    Cleaned data

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,16 +1,34 @@
 puts 'Event Manager Initialized!'
-
+require 'csv'
 fn = 'event_attendees.csv'
 
 # p File.exist? "event_attendees.csv"
 
-contents = File.read(fn)
+# contents = File.read(fn)
 # puts contents
 
-lines = File.readlines(fn)
+# lines = File.readlines(fn)
 
-lines.each do |line|
-    next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
-    columns = line.split(",")
-    p columns[2]
-end
+# row_index = 0
+# lines.each_with_index do |line,index|
+#     next if index == 0
+#     columns = line.split(",")
+#     p line
+#     p columns
+# end
+
+contents = CSV.open(fn, headers: true, header_converters: :symbol)
+
+contents.each do |row|
+    name = row[:first_name]
+    zipcode = row[:zipcode]
+		if zipcode.nil?
+			zipcode = '00000'
+			elsif zipcode.length < 5
+        zipcode = zipcode.rjust(5, '0')
+      elsif zipcode.length > 5
+        zipcode = zipcode[0..4]
+      end
+    p "#{name} #{zipcode}"
+end 
+
