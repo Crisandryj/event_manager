2e50d922e29ee1fbf830252b5e9e18e4e82254e8
Crisandry Javier <crisandryj@gmail.com>
2022-04-27 19:12:56 -0400

    cleaned code

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,62 +3,55 @@ require 'google/apis/civicinfo_v2'
 require 'erb'
 require 'time'
 
-	
-
 def clean_zipcode(zipcode)
-
-zipcode.to_s.rjust(5,"0")[0..4]
-
-end 
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
 
 def legislators_by_zip(zip)
-	civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-	civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
-	begin
-	legislators = civic_info.representative_info_by_address(
-		address: zip, 
-		levels: 'country', 
-		roles: ['legislatorUpperBody', 'legislatorLowerBody']).officials
-	rescue
-		"Please find on website" 
-	end
-end 
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    ).officials
+  rescue StandardError
+    'Please find on website'
+  end
+end
 
-def create_form(id,form)
-  Dir.mkdir("output") unless Dir.exist?('output')
+def create_form(id, form)
+  Dir.mkdir('output') unless Dir.exist?('output')
 
   filename = "output/thanks_#{id}.html"
 
-  File.open(filename,'w') do |file| 
+  File.open(filename, 'w') do |file|
     file.puts form
-  end 
-end 
+  end
+end
 
 def clean_phone_number(phone_num)
   if phone_num.length < 10
-    phone_num = "0000000000"
+    phone_num = '0000000000'
   elsif phone_num.length == 10
     phone_num = phone_num
   elsif phone_num.length == 11 && phone_num[0] == 1
     phone_num = phone_num[1..10]
   elsif phone_num.length == 11 && phone_num[0] != 1
-    phone_num = "0000000000"
+    phone_num = '0000000000'
   elsif phone_num.length > 11
-    phone_num = "0000000000"
-  end 
+    phone_num = '0000000000'
+  end
   phone_num
-end 
+end
 
-
-
-  def frequency(array)
-    array.reduce(Hash.new(0)) do |hash,value|
-      hash[value] += 1
-      hash
-    end 
-  end 
-
+def frequency(array)
+  array.each_with_object(Hash.new(0)) do |value, hash|
+    hash[value] += 1
+  end
+end
 
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 
@@ -67,29 +60,29 @@ erb_template = ERB.new template_letter
 week_day = []
 hour = []
 contents.each do |row|
-	name = row[:first_name]
-	zipcode = clean_zipcode(row [:zipcode])
-	id = row[0]
-  regdate= row[:regdate]
-  phone_num = row[:homephone].gsub(/[^0-9]/, '')  
+  name = row[:first_name]
+  zipcode = clean_zipcode(row [:zipcode])
+  id = row[0]
+  regdate = row[:regdate]
+  phone_num = row[:homephone].gsub(/[^0-9]/, '')
 
   phone_num_list = clean_phone_number(phone_num)
 
   phone_num_list
 
-  reg_date_to_print = DateTime.strptime(regdate,"%m/%d/%y %H:%M")
+  reg_date_to_print = DateTime.strptime(regdate, '%m/%d/%y %H:%M')
 
   hour_of_day = reg_date_to_print.hour
   weekday = reg_date_to_print.wday
   week_day.push(weekday)
   hour.push(hour_of_day)
 
-	legislators = legislators_by_zip(zipcode)
+  legislators = legislators_by_zip(zipcode)
 
-	form_letter = erb_template.result(binding)
+  form_letter = erb_template.result(binding)
 
-  create_form(id,form_letter)
-end 
+  create_form(id, form_letter)
+end
 
-p frequency(week_day).sort_by {|k,v| v}
-p frequency(hour).sort_by {|k,v| v}
+p frequency(week_day).sort_by { |_k, v| v }
+p frequency(hour).sort_by { |_k, v| v }
