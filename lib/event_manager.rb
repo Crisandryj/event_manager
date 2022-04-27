3b2a729dd68665395264bd06ee07ae73f6cd7b67
Crisandry Javier <crisandryj@gmail.com>
2022-04-27 13:18:24 -0400

    Clean phone number method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -24,6 +24,33 @@ def legislators_by_zip(zip)
 	end
 end 
 
+def create_form(id,form)
+  Dir.mkdir("output") unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file| 
+    file.puts form
+  end 
+end 
+
+def clean_phone_number(phone_num)
+  if phone_num.length < 10
+    phone_num = "0000000000"
+  elsif phone_num.length == 10
+    phone_num = phone_num
+  elsif phone_num.length == 11 && phone_num[0] == 1
+    phone_num = phone_num[1..10]
+  elsif phone_num.length == 11 && phone_num[0] != 1
+    phone_num = "0000000000"
+  elsif phone_num.length > 11
+    phone_num = "0000000000"
+  end 
+  phone_num
+end 
+
+
+
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 
 template_letter = File.read('form_letter.erb')
@@ -32,9 +59,19 @@ erb_template = ERB.new template_letter
 contents.each do |row|
 	name = row[:first_name]
 	zipcode = clean_zipcode(row [:zipcode])
+	id = row[0]
+
+  phone_num = row[:homephone].gsub(/[^0-9]/, '')
+
+  phone_num_list = clean_phone_number(phone_num)
+
+  p phone_num_list
 
 	legislators = legislators_by_zip(zipcode)
 
 	form_letter = erb_template.result(binding)
-  puts form_letter
+
+  create_form(id,form_letter)
+
+
 end 
