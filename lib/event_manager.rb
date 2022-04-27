023bb89107e0f29acd9e581705640c1c206f5d82
Crisandry Javier <crisandryj@gmail.com>
2022-04-27 11:00:59 -0400

    working with erb template

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,47 +1,40 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
+	
 
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5, '0')[0..4]
-end
 
-def legislators_by_zipcode(zip)
-	civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-	civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'				
+zipcode.to_s.rjust(5,"0")[0..4]
 
-begin
-
-legislators = civic_info.representative_info_by_address(
-	address: zip,
-	levels: 'country',
-	roles: ['legislatorUpperBody', 'legislatorLowerBody']
-)
-legislators = legislators.officials
-legislator_names = legislators.map{|legislator| legislator.name}
-legislator_names.join(", ")
-
-rescue
-'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-end
 end 
 
-puts 'EventManager initialized.'
+def legislators_by_zip(zip)
+	civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+	civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
-contents = CSV.open(
-  'event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
+	begin
+	legislators = civic_info.representative_info_by_address(
+		address: zip, 
+		levels: 'country', 
+		roles: ['legislatorUpperBody', 'legislatorLowerBody']).officials
+
+		legislators_name = legislators.map {|legislator| legislator.name}
+		legislators_string = legislators_name.join(",")
+
+	rescue
+		"Please find on website" 
+	end
+end 
+
+contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 
 contents.each do |row|
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
+	name = row[:first_name]
+	zipcode = clean_zipcode(row [:zipcode])
 
-	legislators =	legislators_by_zipcode(zipcode)
+	legislators = legislators_by_zip(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
-end
-
-
-template_name = File.read('form_letter.html')
+	puts "#{name} #{zipcode} #{legislators}"
+end 
